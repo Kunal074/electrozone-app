55fbf322eb1f08f6d9086558ef5d51d0f94b0ce1
Kunal074 <kunalsahu232777@gmail.com>
2026-03-22 10:32:52 +0530

    env edit

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,17 +1,27 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const useAuthStore = create((set) => ({
-  user:        null,
-  accessToken: null,
-  isLoggedIn:  false,
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user:        null,
+      accessToken: null,
+      isLoggedIn:  false,
 
-  login: (userData, token) => {
-    set({ user: userData, accessToken: token, isLoggedIn: true });
-  },
+      login: (userData, token) => {
+        set({ user: userData, accessToken: token, isLoggedIn: true });
+      },
 
-  logout: () => {
-    set({ user: null, accessToken: null, isLoggedIn: false });
-  },
-}));
+      logout: () => {
+        set({ user: null, accessToken: null, isLoggedIn: false });
+      },
+    }),
+    {
+      name:    "auth-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
 
 export default useAuthStore;
